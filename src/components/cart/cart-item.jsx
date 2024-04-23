c2d1088c1a249b793ffbcf6d363d089f7f03b054
Aeden Manell <155690067+Aeden-Manell@users.noreply.github.com>
2024-04-23 12:24:26 +0200

    Update cart-item.jsx

--- src/components/cart/cart-item.jsx
+++ src/components/cart/cart-item.jsx
@@ -1,29 +1,45 @@
 import React, { useContext } from "react";
 import { AppleStoreContext } from "../context/AppleStoreContext";
 
+// Functional component responsible for rendering individual items in the cart
 export const CartItem = (props) => {
+  // Destructuring props to extract data for each item
   const { id, productName, price, productImage, briefdescription, color } = props.data;
-  const { cartItems, addToCart, removeFromCart, updateCartItemCount } =
-    useContext(AppleStoreContext);
+
+  // Accessing cart-related functions and state from the AppleStoreContext using the useContext hook
+  const { cartItems, addToCart, removeFromCart, updateCartItemCount } = useContext(AppleStoreContext);
 
   return (
     <div className="cartItem" id="productItem">
-
+      {/* Rendering product image */}
       <img src={productImage} className="product-img" />
+
+      {/* Container for product description */}
       <div className="description" id="description">
+        {/* Rendering product name */}
         <p className="product-name">{productName}</p>
+        {/* Rendering product color */}
         <p className="product-color">{color}</p>
-        <p className="brief-description"> {briefdescription}</p>
+        {/* Rendering brief product description */}
+        <p className="brief-description">{briefdescription}</p>
+        {/* Rendering product price and quantity */}
         <p className="product-price"> Price: ${price} x {cartItems[id]}</p>
+
+        {/* Container for handling quantity modification */}
         <div className="countHandler">
+          {/* Button to decrease quantity */}
           <button onClick={() => removeFromCart(id)} id="subtraction-icon"> - </button>
-          <input id="inputValue"
-            value={cartItems[id]}
-            onChange={(e) => updateCartItemCount(Number(e.target.value), id)}
+          {/* Input field to display and modify quantity */}
+          <input
+            id="inputValue"
+            value={cartItems[id]} // Displaying current quantity
+            onChange={(e) => updateCartItemCount(Number(e.target.value), id)} // Updating quantity on change
           />
-          <button onClick={() => addToCart(id)} id="plus-icon"> + </button>        
+          {/* Button to increase quantity */}
+          <button onClick={() => addToCart(id)} id="plus-icon"> + </button>
         </div>
       </div>
     </div>
   );
 };
+
